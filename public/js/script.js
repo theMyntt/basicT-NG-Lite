8ff4d413681bc06a37722d90d835d94bf7cb0f4c
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-22 18:16:30 -0300

    feat: making login request

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,33 @@
-const loginButton = $('#login-btn')
+const loginButton = $('#login-btn');
 
-const hashText = (text) => {
-  return CryptoJS.SHA256(text).toString(CryptoJS.enc.Base64)
+const hashText = async (text) => {
+  const msgBuffer = new TextEncoder().encode(text);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
 }
 
-loginButton.on("click", () => {
-  const email = $('#email').val()
-  const password = hashText($('#password').val())
+loginButton.on("click", async () => {
+  const email = $('#email').val().toLowerCase();
+  const password = await hashText($('#password').val()); 
 
-  if (!email || !password) return
-})
+  if (!email || !password) return;
+
+  $.ajax({
+    type: 'POST',
+    url: '/api/user/login',
+    data: JSON.stringify({
+      email: email,
+      password: password
+    }),
+    success: () => {
+      alert("Logado com sucesso!");
+    },
+    error: (err) => {
+      alert("Erro ao fazer login: " + err.responseJSON.message);
+    },
+    contentType: 'application/json',
+    dataType: 'json'
+  });
+});
