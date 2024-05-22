0208c22aa3d5f70c4ae97f5836166efdb53e9239
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-22 17:30:04 -0300

    feat: register new user route

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ const path = require('path')
 const hbs = require('hbs')
 const cors = require('cors')
 
+const { generateUniqueId } = require('./utils/')
+const { ProductModel, PurchaseModel, UserModel } = require('./data/connection')
+
 const app = express()
 const port = 1023
 
@@ -16,6 +19,27 @@ app.set('views', path.join(__dirname, 'views'))
 // HBS Templates
 app.get('/', (req, res) => {
   res.render('login')
+})
+
+// Api Routes
+app.post('/api/user/register', async (req, res) => {
+  if (!req) return res.status(400).json(null)
+
+  if (UserModel.find(req.body.email)) return res.status(401).json(null)
+
+  const user = new UserModel({
+    _id: generateUniqueId(),
+    email: req.body.email,
+    password: req.body.password,
+    cellphone: req.body.cellphone,
+    isAdmin: req.body.isAdmin
+  });
+
+  void await user.save()
+
+  return res.status(200).json({
+    message: 'OK'
+  })
 })
 
 app.listen(port, () => {
