f35c030e34721cca7cd16f2fa7b3ba4c4b12825b
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-22 17:51:00 -0300

    feat: sha256 encrypt

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,12 @@
 const loginButton = $('#login-btn')
 
+const hashText = (text) => {
+  return CryptoJS.SHA256(text).toString(CryptoJS.enc.Base64)
+}
+
 loginButton.on("click", () => {
   const email = $('#email').val()
-  const password = $('#password').val()
+  const password = hashText($('#password').val())
 
   if (!email || !password) return
 })
