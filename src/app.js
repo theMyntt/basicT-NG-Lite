5b591f6e2b56da63f6b0bf1e4f44d2182b4dd09b
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-21 22:25:22 -0300

    refactor: window title

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    title: 'basicT NG',
     webPreferences: {
       nodeIntegration: true
     }
