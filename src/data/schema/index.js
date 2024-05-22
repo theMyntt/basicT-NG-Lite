cbc18ed32add6c62cb7279e8e4936adf02eee332
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-22 17:15:57 -0300

    chore: export archive

---
+++ src/data/schema/index.js
@@ -0,0 +1,9 @@
+const ProductModel = require('./product')
+const PurchaseModel = require('./purchase')
+const UserModel = require('./user')
+
+module.exports = {
+  ProductModel,
+  PurchaseModel,
+  UserModel,
+}
