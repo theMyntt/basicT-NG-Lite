5c1652e8c3e8271d4f4578909c7190c5f240e92b
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-21 22:41:32 -0300

    feat: basic login screen configuration

--- src/server.js
+++ src/server.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const cors = require('cors')
 
 const app = express()
 const port = 1023
 
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "../public")))
+
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+// HBS Templates
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('login')
 })
 
 app.listen(port, () => {
