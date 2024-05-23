ab297f30dc4122bd7879a7d7c6e873043c48c6bf
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-22 21:11:01 -0300

    feat: saveCookie function

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,25 @@
-const loginButton = $('#login-btn');
+const loginButton = $('#login-btn')
 
 const hashText = async (text) => {
-  const msgBuffer = new TextEncoder().encode(text);
-  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-  return hashHex;
+  const msgBuffer = new TextEncoder().encode(text)
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer)
+  const hashArray = Array.from(new Uint8Array(hashBuffer))
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
+  return hashHex
 }
 
-loginButton.on("click", async () => {
-  const email = $('#email').val().toLowerCase();
-  const password = await hashText($('#password').val()); 
+const setCookie = (name, value, days) => {
+  var date = new Date()
+  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000))
+  var expires = "expires=" + date.toUTCString()
+  document.cookie = name + "=" + value + "" + expires + "path=/"
+}
 
-  if (!email || !password) return;
+const loginRequest = async () => {
+  const email = $('#email').val().toLowerCase()
+  const password = await hashText($('#password').val()) 
+
+  if (!email || !password) return
 
   $.ajax({
     type: 'POST',
@@ -21,13 +28,43 @@ loginButton.on("click", async () => {
       email: email,
       password: password
     }),
-    success: () => {
-      alert("Logado com sucesso!");
+    success: (response) => {
+      localStorage.clear()
+
+      let i = 1
+      for (let token in response.tokens) {
+        setCookie(`basiclite:token-${i}`, response.tokens[token], 7)
+        i++
+      }
+
+      if (response.isAdmin) {
+        setCookie("basiclite:admin", response.isAdmin)
+      }
+
+      localStorage.setItem("name", response.name)
     },
     error: (err) => {
-      alert("Erro ao fazer login: " + err.responseJSON.message);
+      alert("Erro ao fazer login: " + err.responseJSON.message)
     },
     contentType: 'application/json',
     dataType: 'json'
-  });
-});
+  })
+}
+
+$(document).ready(() => {
+  const title = document.title
+
+  if (title != 'basicT | Login') return
+
+  $(document).on('keydown', (e) => {
+    const { originalEvent } = e;
+    
+    if (originalEvent.key == 'Enter') {
+      loginRequest()
+    }
+  })
+}) 
+
+loginButton.on("click", async () => {
+  loginRequest()
+})
