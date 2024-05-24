6b0dfd4453c09eda01c160223ce138b71bd3706e
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-24 13:29:35 -0300

    feat: delete all cookies when exit the program

--- public/js/script.js
+++ public/js/script.js
@@ -112,4 +112,20 @@ $(document).ready(() => {
 
   if (title == 'basicT | Login') loginMethods()
   if (title == 'basicT | Inicio') homeMethods()
+
+  $("#exit-btn").on('click', () => {
+
+    localStorage.clear()
+
+    const cookies = document.cookie.split(';')
+
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i]
+      const eqPos = cookie.indexOf('=')
+      const name = eqPos > -1? cookie.substr(0, eqPos) : cookie
+      document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT'
+    }
+
+    location.href = '/'
+  })
 }) 
