f231eae95578269ee29b88a32c9cdddeed435139
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-24 13:14:53 -0300

    chore: only cookie name and value

--- public/js/script.js
+++ public/js/script.js
@@ -35,7 +35,7 @@ const setCookie = (name, value, days) => {
   var date = new Date()
   date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000))
   var expires = "expires=" + date.toUTCString()
-  document.cookie = name + "=" + value + "" + expires + "path=/"
+  document.cookie = name + "=" + value
 }
 
 const loginGuard = () => {
